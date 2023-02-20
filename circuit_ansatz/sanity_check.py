8c6b35c1be7e59297b3e5eb82a3cff1567b334c4
Diptanshu Sikdar <dssikdar@gmail.com>
2023-02-20 14:53:30 -0800

    created sanity checker

---
+++ circuit_ansatz/sanity_check.py
@@ -0,0 +1,32 @@
+## Imports
+#from gate_configs import circuit
+
+## Construct a quantum circuit with # of qubits
+#qc = circuit(4)
+
+## Construct a Walsh-Hadamard Transform (H-cascade)
+#qc.build_cascade_hadamard()
+
+## Construct a RX-Gate Cascade
+#qc.build_cascade_rx()
+
+## Construct a RZ-Gate Cascade
+#qc.build_cascade_rz()
+
+## Construct an all-neighbors configuration of CNOT gates
+#qc.cx_all_neighbors()
+
+## Construct an one-to-all configuration of CNOT gates
+#qc.cx_one_to_all()
+
+## Construct an all-neighbors configuration of Controlled-RX gates
+#qc.crx_all_neighbors()
+
+## Construct an one-to-all configuration of Controlled-RX gates
+#qc.crx_one_to_all()
+
+## Draw the quantum circuit
+#print(qc._qc.draw())
+
+## Print all circuit rotation parameters
+#print(qc.all_gate_params)
