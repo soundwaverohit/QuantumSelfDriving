d05fe8ce60e2bf8da9817597d120e9a622d4b639
Diptanshu Sikdar <dssikdar@gmail.com>
2023-02-20 20:51:28 -0800

    create circuit class, initial attributes, getter methods

--- circuit_ansatz/gate_configurations.py
+++ circuit_ansatz/gate_configurations.py
@@ -9,3 +9,20 @@ from qiskit.providers.aer import QasmSimulator
 # Importing other libraries
 import numpy as np
 import random
+
+class circuit:   
+    def __init__(self, num_of_qubits, num_of_clbits=None): 
+        self._num_of_qubits = num_of_qubits 
+        self._all_gate_params = {} 
+        self._gate_id = 0 
+            
+        if num_of_clbits is None: 
+            num_of_clbits = num_of_qubits 
+             
+        self._qc = QuantumCircuit(num_of_qubits, num_of_clbits)
+        
+    def get_quantum_circuit(self):
+        return self._qc
+    
+    def get_rotation_params(self):
+        return self._all_gate_params
