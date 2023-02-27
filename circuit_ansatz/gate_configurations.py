3f79749d1d6c6f773fcd424c08d4b293d4cd60d7
soundwaverohit <soundwave.rohit@gmail.com>
2023-02-26 21:29:57 -0800

    env

--- circuit_ansatz/gate_configurations.py
+++ circuit_ansatz/gate_configurations.py
@@ -4,7 +4,6 @@
 from qiskit import QuantumCircuit, transpile, Aer, IBMQ, assemble, execute, QuantumRegister, ClassicalRegister
 from qiskit.tools.jupyter import *
 from qiskit.visualization import *
-from qiskit.providers.aer import QasmSimulator
 
 # Importing other libraries
 import numpy as np
