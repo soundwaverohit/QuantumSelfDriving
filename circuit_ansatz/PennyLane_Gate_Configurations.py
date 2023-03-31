9e0e8d5072bc8303257ff6336d8475010141e146
Diptanshu <dssikdar@gmail.com>
2023-03-30 22:02:58 -0700

    more changes; not fully functional - WIP

--- circuit_ansatz/PennyLane_Gate_Configurations.py
+++ circuit_ansatz/PennyLane_Gate_Configurations.py
@@ -1,26 +1,56 @@
-# Importing standard Qiskit libraries
+# Importing necessary libraries
 import pennylane as qml
-
-# Importing other libraries
 import numpy as np
-import random
-    
+
+'''
+## REFERENCE CODE ##
+def data_encoding(inputs):
+    for index, data_input in enumerate(inputs):
+        qml.RX(data_input, wires=index)
+
+def quantum_black_box(weights, n_qubits):
+    for idx in range(n_qubits-1):
+        qml.CNOT(wires=[idx, idx+1])
+    for idx in range(n_qubits): 
+        qml.RY(weights[idx], wires=idx)
+'''
+
+
+# Non-parameterized Unary Gate Cascade Arrangements #
+# ------------------------------------------------- #
+
 # applies the hadamard gate to every qubit
 def build_cascade_hadamard(n_qubits): 
     for idx in range(n_qubits): 
         qml.H(idx)
+        
+# applies the NOT gate to every qubit
+def build_cascade_not(n_qubits): 
+    for idx in range(n_qubits): 
+        qml.X(idx)
+        
+        
+# Unary Rotation Gate Cascade Arrangements #
+# ---------------------------------------- #
 
-# applies the rx gate to every qubit        
-def build_cascade_rx(n_qubits, params):
+# applies the RX gate to every qubit        
+def build_cascade_rx(weights, n_qubits):
     for idx in range(n_qubits):
-        qml.RX(params[idx], idx)
-        all_gate_params['w'+str(gate_id)] = params[idx]
+        qml.RX(weights[idx], wires=idx)
 
-# applies the rz gate to every qubit        
-def build_cascade_rz(n_qubits, params=None):
+# applies the RY gate to every qubit  
+def build_cascade_rx(weights, n_qubits):
     for idx in range(n_qubits):
-        qml.RZ(params[idx], idx)
-        all_gate_params['w'+str(gate_id)] = params[idx]
+        qml.RY(weights[idx], wires=idx)
+
+# applies the RZ gate to every qubit        
+def build_cascade_rz(weights, n_qubits):
+    for idx in range(n_qubits):
+        qml.RZ(weights[idx], wires=idx)
+        
+        
+# Non-parameterized Controlled Gate Cascade Arrangements #
+# ------------------------------------------------------ #
 
 def cx_all_neighbors(n_qubits):
     for i in range(n_qubits):
@@ -31,13 +61,13 @@ def cx_all_neighbors(n_qubits):
         if control == n_qubits - 1:
             target = 0
 
-        qml.cx(control, target)
+        qml.CNOT(wires=[control, target])
 
 def cx_almost_all_neighbors(n_qubits):
-    for i in range(n_qubits-1):
-        control = i
-        target = i+1                
-        qml.cx(control, target)
+    for idx in range(n_qubits-1):
+        control = idx
+        target = idx+1                
+        qml.CNOT(wires=[control, target])
 
 def cx_one_to_all(n_qubits):       
     index_list = [i for i in range(n_qubits)]
@@ -47,74 +77,59 @@ def cx_one_to_all(n_qubits):
         temp_index_list = index_list.copy()
         temp_index_list.remove(control)
         for target in temp_index_list:
-            qml.cx(control, target)
+            qml.CNOT(wires=[control, target])
+            
+            
+# Controlled Rotation Gate Cascade Arrangements #
+# --------------------------------------------- #
 
-def crx_all_neighbors(n_qubits, params=None):
-    if params == None:
-        params = [random.uniform(0, 2*np.pi) for i in range(n_qubits)]
-
+def crx_all_neighbors(weights, n_qubits):
     for i in range(n_qubits):
         control = i
         target = i+1
         if control == n_qubits - 1:
             target = 0
-        qml.crx(params[i], control, target)
-        all_gate_params['w'+str(gate_id)] = params[i]
+        qml.crx(weights[i], control, target)
+        
 
-def crx_almost_all_neighbors(n_qubits, params=None):
-    if params == None:
-        params = [random.uniform(0, 2*np.pi) for i in range(n_qubits)]
-
+def crx_almost_all_neighbors(weights, n_qubits):
     for i in range(n_qubits - 1):
         control = i
         target = i+1
-        qml.crx(params[i], control, target)
-        all_gate_params['w'+str(gate_id)] = params[i]
+        qml.crx(weights[i], control, target)
+        
 
-def crx_one_to_all(n_qubits, params):
+def crx_one_to_all(weights, n_qubits):
     index_list = [i for i in range(n_qubits)]
-    if params == None:
-        params = [random.uniform(0, 2*np.pi) for i in range(n_qubits * (n_qubits-1))]
-
     for j in range(n_qubits):
         control = j
         temp_index_list = index_list.copy()
         temp_index_list.remove(control)
         for target in temp_index_list:
-            qml.crx(params[j+target], control, target)
-            all_gate_params['w'+str(gate_id)] = params[j+target]
+            qml.crx(weights[j+target], control, target)
+            all_gate_weights['w'+str(gate_id)] = weights[j+target]
 
-def crz_all_neighbors(n_qubits, params):
-    if params == None:
-        params = [random.uniform(0, 2*np.pi) for i in range(n_qubits)]
-
+def crz_all_neighbors(weights, n_qubits):
     for i in range(n_qubits):
         control = i
         target = i+1
         if control == n_qubits - 1:
             target = 0
-        qml.crz(params[i], control, target)
-        all_gate_params['w'+str(gate_id)] = params[i]
+        qml.crz(weights[i], control, target)
+        
 
-def crz_almost_all_neighbors(n_qubits, params):
-    if params == None:
-        params = [random.uniform(0, 2*np.pi) for i in range(n_qubits)]
-
+def crz_almost_all_neighbors(weights, n_qubits):
     for i in range(n_qubits - 1):
         control = i
         target = i+1
-        qml.crz(params[i], control, target)
-        all_gate_params['w'+str(gate_id)] = params[i]
+        qml.crz(weights[i], control, target)
+        
 
-def crz_one_to_all(n_qubits, params):
+def crz_one_to_all(weights, n_qubits):
     index_list = [i for i in range(n_qubits)]
-    if params == None:
-        params = [random.uniform(0, 2*np.pi) for i in range(n_qubits * (n_qubits-1))]
-
     for j in range(n_qubits):
         control = j
         temp_index_list = index_list.copy()
         temp_index_list.remove(control)
         for target in temp_index_list:
-            qml.crz(params[j+target], control, target)
-            all_gate_params['w'+str(gate_id)] = params[j+target]
+            qml.crz(weights[j+target], control, target)
