0db3821f36f877fc96a30ea1f5f1211a9f7016c6
Diptanshu Sikdar <69136009+dssikdar@users.noreply.github.com>
2023-04-01 09:23:45 -0700

    Not needed

--- circuit_ansatz/circuit_5.py
+++
@@ -1,8 +0,0 @@
-from gate_configurations import circuit
-
-circ_5 = circuit(4)
-circ_5.build_cascade_rx()
-circ_5.build_cascade_rz()
-circ_5.crz_one_to_all()
-circ_5.build_cascade_rx()
-circ_5.build_cascade_rz()
