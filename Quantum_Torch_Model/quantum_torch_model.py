bfc0ae9958098bc4553051106c5549c3b5306cf5
Rohit Ganti <soundwave.rohit@gmail.com>
2023-11-22 06:05:16 -0500

    Create quantum_torch_model.py

---
+++ Quantum_Torch_Model/quantum_torch_model.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+import pennylane as qml
+import numpy as np
+
+# Define a quantum device - can be a simulator or real quantum hardware
+device = qml.device("default.qubit", wires=4)
+
+# Define a quantum circuit
+def quantum_circuit(inputs, weights):
+    qml.templates.AngleEmbedding(inputs, wires=range(4))
+    qml.templates.BasicEntanglerLayers(weights, wires=range(4))
+    return [qml.expval(qml.PauliZ(i)) for i in range(4)]
+
+# Wrap the quantum circuit in a qnode
+@qml.qnode(device)
+def qnode(inputs, weights):
+    return quantum_circuit(inputs, weights)
+
+# Define the shape of the weights for each layer in the quantum circuit
+n_layers = 3
+weight_shapes = {"weights": (n_layers, 4)}
+
+# Create the quantum layer
+quantum_layer = qml.qnn.TorchLayer(qnode, weight_shapes)
+
+class QuantumModel(nn.Module):
+    def __init__(self):
+        super(QuantumModel, self).__init__()
+        # [Your classical layers...]
+        self.fc1 = nn.Linear(39600, 256)  # Adjust input size as needed
+        self.fc2 = nn.Linear(256, 4)      # Layer to match quantum input size
+        self.relu = nn.ReLU()
+        # Quantum layer
+        self.q_layer = quantum_layer
+        # Output layer
+        self.fc3 = nn.Linear(4, 10)       # Output size depends on your application
+
+    def forward(self, x):
+        # [Your forward pass...]
+        x = x.reshape(-1, 39600)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.q_layer(x)
+        x = self.fc3(x)
+        return x
+
+# Example usage
+# model = QuantumModel()
